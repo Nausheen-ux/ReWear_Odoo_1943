e4c029acba25512689e20aa753541b53d546f031
Prachi Rattan <143030430+PrachiRattan@users.noreply.github.com>
2025-07-12 12:08:17 +0530

    Delete routes directory

--- routes/itemRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { auth } = require('../middleware/auth');
-const { addItem, getItems, getItemById } = require('../controllers/itemController');
-router.post('/add', auth, addItem);
-router.get('/', getItems); // for browsing
-router.get('/:id', getItemById);
-module.exports = router;
-
