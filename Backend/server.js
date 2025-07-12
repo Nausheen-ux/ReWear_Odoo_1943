91f8ca3d54661a3ec69eddcd3bf8071e854d5430
Prachi Rattan <143030430+PrachiRattan@users.noreply.github.com>
2025-07-12 13:54:13 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,9 +7,12 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/items', require('./routes/itemRoutes'));
